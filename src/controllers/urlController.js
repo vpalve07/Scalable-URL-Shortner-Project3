ccf77ebe58385c226746c2c22a7e4b0bcd659fcd
vpalve07 <vinayak.palve02@gmail.com>
2023-01-17 21:44:44 +0530

    pro3 commit

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,12 +1,18 @@
 const urlModel = require('../modules/urlModel')
 const shortid = require('shortid')
-const validUrl = require('valid-url')
 
 const url = async function (req, res) {
     try {
         let data = req.body
-        let longUrl = data.longUrl
-        if (!validUrl.isWebUri(longUrl)) return res.status(400).send({ status: false, msg: "Please Enter Valid Url" })
+        let {longUrl,shortUrl,urlCode} = data
+        if(Object.keys(data).length==0) return res.status(400).send({status:false,msg:"Request Body cant be empty"})
+        // if(Object.keys(data).includes()) return res.status(400).send({status:false,msg:"Request body can only contain 'longUrl','shortUrl','urlCode'"})
+        if(!Object.keys(data).includes('longUrl')) return res.status(400).send({status:false,msg:"longUrl should be there in request body"})
+        if(Object.keys(data).includes('shortUrl')) return res.status(400).send({status:false,msg:"shortUrl should not be there in request body"})
+        if(Object.keys(data).includes('urlCode')) return res.status(400).send({status:false,msg:"urlCode should not be there in request body"})
+        
+        let LinkFormat = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%\+.~#?&\/=]*)$/
+        if (!LinkFormat.test(longUrl)) return res.status(400).send({ status: false, msg: "Please Enter Valid Url" })
         const findUrl = await urlModel.findOne({ longUrl: longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
 
         if (findUrl) { return res.status(200).send({ status: true, data: { longUrl: findUrl.longUrl, shortUrl: findUrl.shortUrl, urlCode: findUrl.urlCode } }) };
@@ -14,7 +20,6 @@ const url = async function (req, res) {
         let baseUrl = `${req.protocol}://${req.get('host')}/`
         data.urlCode = shortUrlCode
         data.shortUrl = baseUrl + shortUrlCode
-        let { shortUrl, urlCode } = data
         let createData = await urlModel.create(data)
         return res.status(201).send({ status: true, data: { longUrl, shortUrl, urlCode } })
     } catch (error) {
@@ -24,10 +29,14 @@ const url = async function (req, res) {
 }
 
 const getUrl = async function (req, res) {
-    let urlCode = req.params.urlCode
-    let getData = await urlModel.findOne({ urlCode: urlCode }).select({ urlCode: 1, shortUrl: 1, longUrl: 1, _id: 0 })
-    if (!getData) return res.status(400).send({ status: false, msg: "Page Not found" })
-    return res.status(302).redirect(getData.longUrl)
+    try {
+        let urlCode = req.params.urlCode
+        let getData = await urlModel.findOne({ urlCode: urlCode }).select({ urlCode: 1, shortUrl: 1, longUrl: 1, _id: 0 })
+        if (!getData) return res.status(400).send({ status: false, msg: "Page Not found" })
+        return res.status(302).redirect(getData.longUrl)
+    } catch (error) {
+        return res.send({ errorType: error.name, errorMsg: error.message })
+    }
 }
 
 module.exports = { url, getUrl }
