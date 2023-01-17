11ff9984b7633921573403ba4da5140c0be28991
vpalve07 <vinayak.palve02@gmail.com>
2023-01-17 18:50:50 +0530

    pro3 commit

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,33 +1,33 @@
 const urlModel = require('../modules/urlModel')
 const shortid = require('shortid')
+const validUrl = require('valid-url')
 
-const url = async function(req,res){
-  try {
-      let data = req.body
-      let longUrl = data.longUrl
-      let LinkFormat = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%\+.~#?&\/=]*)$/
-      if (!LinkFormat.test(longUrl)) return res.status(400).send({ status: false, msg: "Please Enter Valid Url" })
-      const findUrl = await urlModel.findOne({ longUrl: longUrl });
+const url = async function (req, res) {
+    try {
+        let data = req.body
+        let longUrl = data.longUrl
+        if (!validUrl.isWebUri(longUrl)) return res.status(400).send({ status: false, msg: "Please Enter Valid Url" })
+        const findUrl = await urlModel.findOne({ longUrl: longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
 
-    if (findUrl) { return res.status(200).send({ status: true, data: {longUrl: findUrl.longUrl, shortUrl: findUrl.shortUrl, urlCode: findUrl.urlCode }})};
-      let shortUrlCode = shortid.generate(longUrl)
-      let baseUrl = "http://localhost:3000/"
-      data.urlCode = shortUrlCode
-      data.shortUrl = baseUrl+shortUrlCode
-      let {shortUrl,urlCode} = data
-      let createData = await urlModel.create(data)
-      res.status(201).send({status:true,data:{longUrl,shortUrl,urlCode}})
-  } catch (error) {
-    res.send({errorType:error.name,errorMsg:error.message})
-  }
+        if (findUrl) { return res.status(200).send({ status: true, data: { longUrl: findUrl.longUrl, shortUrl: findUrl.shortUrl, urlCode: findUrl.urlCode } }) };
+        let shortUrlCode = shortid.generate()
+        let baseUrl = `${req.protocol}://${req.get('host')}/`
+        data.urlCode = shortUrlCode
+        data.shortUrl = baseUrl + shortUrlCode
+        let { shortUrl, urlCode } = data
+        let createData = await urlModel.create(data)
+        return res.status(201).send({ status: true, data: { longUrl, shortUrl, urlCode } })
+    } catch (error) {
+        return res.send({ errorType: error.name, errorMsg: error.message })
+    }
 
 }
 
-const getUrl = async function(req,res){
+const getUrl = async function (req, res) {
     let urlCode = req.params.urlCode
-    let getData = await urlModel.findOne({urlCode:urlCode}).select({urlCode:1,shortUrl:1,longUrl:1,_id:0})
-    if(!getData) return res.status(400).send({status:false,msg:"Page Not found"})
-    res.status(302).redirect(getData.longUrl)
+    let getData = await urlModel.findOne({ urlCode: urlCode }).select({ urlCode: 1, shortUrl: 1, longUrl: 1, _id: 0 })
+    if (!getData) return res.status(400).send({ status: false, msg: "Page Not found" })
+    return res.status(302).redirect(getData.longUrl)
 }
 
-module.exports = {url,getUrl}
+module.exports = { url, getUrl }
