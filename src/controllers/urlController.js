7886f756a0caf21c5789fab4238ad0fb897869f5
vpalve07 <vinayak.palve02@gmail.com>
2023-01-19 14:44:17 +0530

    pro3 commit

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,16 +1,16 @@
 const urlModel = require('../modules/urlModel')
 const shortid = require('shortid')
 const redis = require("redis")
-const validUrl = require('validator')
+const axios = require('axios')
 const { promisify } = require("util")
 
 
 //1. Connect to the redis server
 
 const client = redis.createClient({
-    url:'redis://default:RXGCwJUfsFLEWSsJIsiiFJCjdJFNGE0m@redis-18345.c264.ap-south-1-1.ec2.cloud.redislabs.com:18345'
+    url: 'redis://default:RXGCwJUfsFLEWSsJIsiiFJCjdJFNGE0m@redis-18345.c264.ap-south-1-1.ec2.cloud.redislabs.com:18345'
 })
-client.on('error',(err)=>console.log('Redis Client Error',err))
+client.on('error', (err) => console.log('Redis Client Error', err))
 console.log("Connected to Redis..")
 
 //2. Prepare the functions for each command
@@ -28,28 +28,26 @@ const url = async function (req, res) {
         if (!Object.keys(data).includes('longUrl')) return res.status(400).send({ status: false, msg: "'longUrl' should be there in request body" })
         if (Object.keys(data).length > 1) return res.status(400).send({ status: false, msg: "Enter 'longUrl' only in request body" })
 
-        if (!validUrl.isURL(longUrl)) return res.status(400).send({ status: false, msg: "Please Enter Valid Url" })
 
-        // const checkUrl = await axios.get(data)
-        // .then(()=>data)
-        // .catch(()=>null)
-
-        // if(!checkUrl) return res.status(400).send({ status: false, msg: "Please Enter Valid Url" })
+        const response = await axios.get(longUrl)
+            .then(() => longUrl)
+            .catch(() => null)
+        if (!response) return res.status(400).send({ status: false, msg: `The URL ${longUrl} is not valid` })
 
 
         const findUrl = await urlModel.findOne({ longUrl: longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
         if (findUrl) { return res.status(200).send({ status: true, data: { longUrl: findUrl.longUrl, shortUrl: findUrl.shortUrl, urlCode: findUrl.urlCode } }) };
-        
+
         let shortUrlCode = shortid.generate()
         let baseUrl = `${req.protocol}://${req.get('host')}/`
         data.urlCode = shortUrlCode
         data.shortUrl = baseUrl + shortUrlCode
         let { shortUrl, urlCode } = data
         let createData = await urlModel.create(data)
-        await SET_ASYNC(`${shortUrlCode}`, 50,  JSON.stringify(data))
+        await SET_ASYNC(`${shortUrlCode}`, 50, JSON.stringify(data))
         return res.status(201).send({ status: true, data: { longUrl, shortUrl, urlCode } })
     } catch (error) {
-        return res.send({ errorType: error.name, errorMsg: error.message })
+        return res.status(500).send({ errorType: error.name, errorMsg: error.message })
     }
 
 }
@@ -63,11 +61,11 @@ const getUrl = async function (req, res) {
         else {
             let getData = await urlModel.findOne({ urlCode: req.params.urlCode }).select({ urlCode: 1, shortUrl: 1, longUrl: 1, _id: 0 })
             if (!getData) return res.status(400).send({ status: false, msg: "Page Not found" })
-            await SET_ASYNC(`${req.params.urlCode}`, 50,  JSON.stringify(getData))
+            await SET_ASYNC(`${req.params.urlCode}`, 50, JSON.stringify(getData))
             return res.status(302).redirect(getData.longUrl)
         }
     } catch (error) {
-        return res.send({ errorType: error.name, errorMsg: error.message })
+        return res.status(500).send({ errorType: error.name, errorMsg: error.message })
     }
 }
 
