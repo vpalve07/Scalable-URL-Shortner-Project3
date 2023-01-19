accedbbce8cf691e3be7fdb9421d5dec0fce6d5a
vpalve07 <vinayak.palve02@gmail.com>
2023-01-19 22:29:57 +0530

    Final-Commit

--- src/modules/urlModel.js
+++ src/modules/urlModel.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose')
 
 const urlSchema = new mongoose.Schema({
-    urlCode:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true
+    urlCode: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
-    longUrl:{
-        type:String,
-        required:true,
-        trim:true
+    longUrl: {
+        type: String,
+        required: true,
+        trim: true
     },
-    shortUrl:{
-        type:String,
-        required:true,
-        unique:true
+    shortUrl: {
+        type: String,
+        required: true,
+        unique: true
     }
-    
-},{timestamps:true})
 
-module.exports = mongoose.model('URL',urlSchema)
+}, { timestamps: true })
+
+module.exports = mongoose.model('URL', urlSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,13 +2,13 @@ const express = require('express')
 const { url, getUrl } = require('../controllers/urlController')
 const router = express.Router()
 
-router.get('/test-me',function(req,res){
-    res.send({test:"Test-API"})
+router.get('/test-me', function (req, res) {
+    res.send({ test: "Test-API" })
 })
 
-router.post('/url/shorten',url)
-router.get('/:urlCode',getUrl)
-router.all('/*',function(req,res){ return res.status(400).send({msg:"Invalid HTTP request"})})
+router.post('/url/shorten', url)
+router.get('/:urlCode', getUrl)
+router.all('/*', function (req, res) { return res.status(400).send({ msg: "Invalid HTTP request" }) })
 
 
 module.exports = router
