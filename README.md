7ae65b41a8387f51d5ec82ab8210867791cddc5c
vpalve07 <vinayak.palve02@gmail.com>
2023-01-19 13:01:59 +0530

    pro3 commit

--- README.md
+++ README.md
@@ -56,9 +56,15 @@ If you haven’t used tinyurl.com before, please try creating a new shortened UR
 - Each member of each team should have their tests in running state
 
 ## Phase II
-- Use caching while creating the shortened url to minimize db calls.
-- Implement what makes sense to you and we will build understanding over the demo discussion. 
-- Figure out if you can also use caching while redirecting to the original url from the shortedned url
+
+```diff
++Consider that Twitter has this trend where a famous person with a wide following when posts a link, the link gets frequented in millions within a day.
+
++So in our application we would want to implement caching so that a newly created link is cached for 24 hours. When a person uses a short url, the long url should be retrieved from cache in the first 24 hours of that url being created.
+
++- Use caching while fetching the shortened url to minimize db calls.
++- Implement what makes sense to you and we will build understanding over the assessment of this project. You should understand and should be able to explain the logic that you have implemented.
+```
 
 ## Response
 
--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -7,25 +7,16 @@ const { promisify } = require("util")
 
 //1. Connect to the redis server
 
-const redisClient = redis.createClient(
-    18345,
-    "redis-18345.c264.ap-south-1-1.ec2.cloud.redislabs.com",
-    { no_ready_check: true }
-);
-redisClient.auth("RXGCwJUfsFLEWSsJIsiiFJCjdJFNGE0m", function (err) {
-    if (err) throw err;
-});
-
-redisClient.on("connect", async function () {
-    console.log("Connected to Redis..");
-});
-
+const client = redis.createClient({
+    url:'redis://default:RXGCwJUfsFLEWSsJIsiiFJCjdJFNGE0m@redis-18345.c264.ap-south-1-1.ec2.cloud.redislabs.com:18345'
+})
+client.on('error',(err)=>console.log('Redis Client Error',err))
+console.log("Connected to Redis..")
 
 //2. Prepare the functions for each command
 
-const SET_ASYNC = promisify(redisClient.SETEX).bind(redisClient);
-const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
-
+const SET_ASYNC = promisify(client.SETEX).bind(client);
+const GET_ASYNC = promisify(client.GET).bind(client);
 
 
 const url = async function (req, res) {
@@ -37,8 +28,15 @@ const url = async function (req, res) {
         if (!Object.keys(data).includes('longUrl')) return res.status(400).send({ status: false, msg: "'longUrl' should be there in request body" })
         if (Object.keys(data).length > 1) return res.status(400).send({ status: false, msg: "Enter 'longUrl' only in request body" })
 
-        // let LinkFormat = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%\+.~#?&\/=]*)$/
         if (!validUrl.isURL(longUrl)) return res.status(400).send({ status: false, msg: "Please Enter Valid Url" })
+
+        // const checkUrl = await axios.get(data)
+        // .then(()=>data)
+        // .catch(()=>null)
+
+        // if(!checkUrl) return res.status(400).send({ status: false, msg: "Please Enter Valid Url" })
+
+
         const findUrl = await urlModel.findOne({ longUrl: longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
         if (findUrl) { return res.status(200).send({ status: true, data: { longUrl: findUrl.longUrl, shortUrl: findUrl.shortUrl, urlCode: findUrl.urlCode } }) };
         
@@ -60,13 +58,13 @@ const getUrl = async function (req, res) {
     try {
         let cahcedProfileData = await GET_ASYNC(`${req.params.urlCode}`)
         if (cahcedProfileData) {
-            return res.status(302).redirect(JSON.parse(cahcedProfileData).shortUrl)
+            return res.status(302).redirect(JSON.parse(cahcedProfileData).longUrl)
         }
         else {
             let getData = await urlModel.findOne({ urlCode: req.params.urlCode }).select({ urlCode: 1, shortUrl: 1, longUrl: 1, _id: 0 })
             if (!getData) return res.status(400).send({ status: false, msg: "Page Not found" })
-            await SET_ASYNC(`${req.params.urlCode}`, 10,  JSON.stringify(getData))
-            return res.status(302).redirect(JSON.parse(getData).shortUrl)
+            await SET_ASYNC(`${req.params.urlCode}`, 50,  JSON.stringify(getData))
+            return res.status(302).redirect(getData.longUrl)
         }
     } catch (error) {
         return res.send({ errorType: error.name, errorMsg: error.message })
